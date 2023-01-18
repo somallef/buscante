f273e6db54b73f0a97ef6ac90c2235e45ebc21a6
Allef Mizael <allef.mizael@gmail.com>
2023-01-18 15:36:17 -0300

    Criação das interfaces para receber o retorno da API utilizando os operadores pipe e map do rxjs

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+import { Item, LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class LivroService {
 
   constructor(private http: HttpClient) { }
 
-  buscar(termoDePesquisa: string): Observable<any> {
+  buscar(termoDePesquisa: string): Observable<Item[]> {
     const params = new HttpParams().append('q', termoDePesquisa);
 
     //Observable: (observável) coleção de valores ou eventos futuros. 
@@ -21,6 +22,11 @@ export class LivroService {
     //Ele consegue ouvir os valores entregues pelo Observable.
 
     //Padrão Observer ou pub/sub: base da programação orientada a eventos, util quando vários componentes estão interessados em um determinado evento 
-    return this.http.get(this.API, {params});
+    return this.http
+                .get<LivrosResultado>(this.API, {params})
+                //Os operadores, quando conectados a um Observable, executam uma ação e retornam outro Observable.
+                .pipe( //pipe é um operador que canaliza/agrupa um fluxo de outros operadores
+                  tap(retornoAPI => console.log(retornoAPI)), //tap é o operador que permite inspecionar o valor do Observable
+                  map(retornoAPI => retornoAPI.items)); 
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Livro } from 'src/app/models/interfaces';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
@@ -6,24 +8,48 @@ import { LivroService } from 'src/app/service/livro.service';
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[]
   campoBusca: string = ''
+  subscription: Subscription
+  livro: Livro
 
   constructor(private service: LivroService) { }
-
+  
   //Quando um evento acontece, o observable notificará todos os observers e cada observer 
   //puxará as informações de que precisa do observable.
   //O Observer é uma coleção de callbacks que sabe escutar os valores entregues pelo Observable.
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe({ //Assinar um observable é como chamar uma função, fornecendo retornos de chamada para onde os dados serão entregues.
-      next: (retornoDoObservable) => console.log(retornoDoObservable), //cada chave aqui representa um tipo de notificação do Observable
+    this.subscription = this.service.buscar(this.campoBusca).subscribe({ //Assinar um observable é como chamar uma função, fornecendo retornos de chamada para onde os dados serão entregues.
+      next: (retornoDoObservable) => this.listaLivros = this.livrosResultadoParaLivros(retornoDoObservable), //cada chave aqui representa um tipo de notificação do Observable
       error: (erroRetornadoDoObservable) => console.error(erroRetornadoDoObservable),
       complete: () => console.log('Observable completado') //a notificação do tipo complete permite notificar o fim do ciclo de vida do Observable
     });
   }
 
+  livrosResultadoParaLivros(items): Livro[] {
+    
+    const livros: Livro[] = []
+
+    items.forEach(item => {
+      livros.push(this.livro = {
+        title: item.volumeInfo?.title,
+        authors: item.volumeInfo?.authors,
+        publisher: item.volumeInfo?.publisher,
+        publishedDate: item.volumeInfo?.publishedDate,
+        description: item.volumeInfo?.description,
+        previewLink: item.volumeInfo?.previewLink,
+        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
+      })
+    })
+
+    return livros
+  }
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
 
 
